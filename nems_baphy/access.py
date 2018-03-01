6ae0e6a31f47c11ebc2fb0dd698fa766f675bf1a
Ivar Thorson <drrobot@gmail.com>
2018-03-01 13:31:17 -0800

    WIP. Committing a sketch that will use the NEMS repo for DB access. Later, I will move DB.py to this repo so it can be removed from the NEMS repo.

---
+++ nems_baphy/access.py
@@ -0,0 +1,56 @@
+import nems.db as nd
+import nems.utilities.baphy
+
+
+def list_recordings(batch):
+    ''' 
+    Returns a list of all cellids for a given batch.
+    '''
+    cell_data = nd.get_batch_cells(batch=batch)
+    cellids = list(cell_data['cellid'].unique())
+    return cellids
+
+
+def load_recording_from_baphy(batch, cellid, **options):
+    '''
+    Returns a recording object loaded from Baphy.
+    '''
+    if batch in [271]:
+        return _load_from_271(cellid, **options)
+    else:
+        raise NotImplementedError
+
+
+def _load_from_271(cellid,
+                   rasterfs=100,
+                   chancount=18,
+                   includeprestim=True,
+                   pupil=False,
+                   stimfmt='ozgf',
+                   stim=True):
+    '''
+    Returns a Recording loaded from a cell in batch 271.
+    '''
+    batch = 271
+    stimfmt = stimfmt if stim else 'none'
+    options = {'rasterfs': rasterfs,
+               'chancount': chancount,
+               'stimfmt': stimfmt,
+               'includeprestim': includeprestim,
+               'pupil': pupil,
+               'stim': stim}
+    recording = nems.utilities.baphy.baphy_load_recording(cellid, batch, options)
+
+    except:
+        print("failed on cell {0}".format(cellid))    
+
+
+def test_load_recording_from_baphy():
+    pass
+    # # TODO: Recording should contain more info
+    # save_path="/auto/data/tmp/batch{0}_fs{1}_{2}{3}/".format(batch, 
+    #                                                          rasterfs,
+    #                                                          stimfmt,
+    #                                                          chancount])
+
+    # recording.save(save_path)

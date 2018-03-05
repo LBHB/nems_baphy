4fac240d5c6c6d81ea25a047ad5b5a8102ed325e
Ivar Thorson <thorsoni@ohsu.edu>
2018-03-05 11:40:40 -0800

    Revert "Just changing the default port number."
    This reverts commit 0c7e6067ccc2fef36ca9931607e70f30bec6b4de.

--- configs/default_config.sh
+++ configs/default_config.sh
@@ -3,7 +3,7 @@
 # to the git repo, in order to keep your secrets (host, pass, etc) safe.
 
 export NEMS_BAPHY_API_HOST='localhost'
-export NEMS_BAPHY_API_PORT='3002'
+export NEMS_BAPHY_API_PORT='3003'
 export NEMS_RECORDINGS_DIR="/auto/data/recordings/"
 export MYSQL_HOST=
 export MYSQL_USER=

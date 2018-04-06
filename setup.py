4bce84dfc47f9e91032e4c945cd8bfba6a09ed67
Jacob <jacob.pennington@ohsu.edu>
2018-04-06 14:14:52 -0700

    removed old wrappers file, updated setup to remove unnecessary install requirements

--- setup.py
+++ setup.py
@@ -10,12 +10,8 @@ with codecs.open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 GENERAL_REQUIRES = [
-        'flask', 'sqlalchemy', 'mpld3',
-        'flask-socketio', 'eventlet', 'bcrypt', 'flask-restful',
-        'flask-WTF', 'flask-login', 'flask-bcrypt', 'flask-assets',
-        'pymysql', 'gevent',
+        'flask', 'flask-restful',
         ]
-
 
 setup(
     name=NAME,
@@ -28,6 +24,6 @@ setup(
     description='Neural Encoding Model System - Database support',
     long_description=long_description,
     url='http://neuralprediction.org',
-    #install_requires=GENERAL_REQUIRES,
+    install_requires=GENERAL_REQUIRES,
     classifiers=[],
 )
